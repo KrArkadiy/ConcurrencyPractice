c54c9fb57981d63b98e4d1dab178f98081599476
arkadiy krylosov <arkady.krylosov@yandex.ru>
2021-07-12 20:19:12 +0300

    Task realization

--- src/main/java/ConcurrencyPractice.java
+++ src/main/java/ConcurrencyPractice.java
@@ -6,16 +6,11 @@ import java.util.concurrent.Executors;
 public class ConcurrencyPractice {
     public static void main(String[] args) {
         Foo f1 = new Foo();
-        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(()->f1.first(Thread.currentThread()));
-        CompletableFuture<Void> voidCompletableFuture1 = CompletableFuture.runAsync(()->f1.third(Thread.currentThread()));
-        CompletableFuture<Void> voidCompletableFuture2 = CompletableFuture.runAsync(()->f1.second(Thread.currentThread()));
+        CompletableFuture<Void> voidCompletableFuture = CompletableFuture.runAsync(() -> f1.first(Thread.currentThread()));
+        CompletableFuture<Void> voidCompletableFuture1 = CompletableFuture.runAsync(() -> f1.third(Thread.currentThread()));
+        CompletableFuture<Void> voidCompletableFuture2 = CompletableFuture.runAsync(() -> f1.second(Thread.currentThread()));
         CompletableFuture<Void> allFutures = CompletableFuture.allOf(voidCompletableFuture, voidCompletableFuture1, voidCompletableFuture2);
         System.out.println(allFutures);
-
-
-
-
-
     }
 }
 
